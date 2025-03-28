96073ede7de7b2a484ab44e3b794400da5628ffd
Anaquerzia <anaquerziaalice@gmail.com>
2025-03-28 08:52:23 -0300

    Streams, leitura,escrita e transformação

--- streams/fundamentos.js
+++ streams/fundamentos.js
@@ -11,7 +11,7 @@
 //stdout= stream de saida padrão, é uma stream de escrita que é usada para escrever dados no terminal.
 //stderr= stream de erro padrão, é uma stream de escrita que é usada para escrever mensagens de erro no terminal.
 
-import {Readable } from 'node:stream'
+import {Readable, Transform, Writable } from 'node:stream'
 
 class OnetoHundredStream extends Readable{
     index=1
@@ -30,5 +30,31 @@ class OnetoHundredStream extends Readable{
         },1000)
     }
 }
+
+//metodo _write é chamado quando um pedaço de dados é escrito na stream.
+// O parametro chunk é o pedaço de dados que foi escrito, é o que esta sendo enviado dentro do  this.push(buf)
+// O parametro encoding é como os dados foram codificados. 
+// O parametro callback é uma função que deve ser chamada quando a função terminou de fazer o que precisava fazer com a informação.
+
+//Dentro de uma stream de escrita a gente não processa nada,  dentro do _write, a gente só processa ou escreve os dados , 
+//Ela nunca vai transformar um dado , ela vai somente processar o dado e enviar para outro lugar. 
+class MultiplyByTwoStream extends Writable{
+    _write(chunk, encoding, callback) 
+    {
+        console.log(Number(chunk.toString())* 10)
+        callback()
+    }
+}
+
+class inverterNunber  extends Transform{
+    _transform(chunck, encoding, callback)
+    {
+        const transformed = Number(chunck.toString())* -1
+        callback(null,Buffer.from(String(transformed)))
+    }
+}
+//Nesta linha estamos lendo os dados de 1 a 100  da função OneToHundredStream e passando para a função MultiplyByTwoStream
+//A função MultiplyByTwoStream vai pegar os dados da OnetoHundred e  multiplicando  os dados por 10 e imprimindo no console.
 new OnetoHundredStream()
-.pipe(process.stdout)
+.pipe(new inverterNunber())
+.pipe( new MultiplyByTwoStream())
